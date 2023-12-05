fb4e01113daf8c54a78f3d74164ebc32e054a879
rina0uchi <34464831+rina0uchi@users.noreply.github.com>
2023-12-05 13:57:26 +0900

    fixed: setuptools_scm

--- src/pydate_generator/__init__.py
+++ src/pydate_generator/__init__.py
@@ -1,1 +1,8 @@
 from .pydate_generator import *
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pydate_generator")
+except PackageNotFoundError:
+    # package is not installed
+    pass

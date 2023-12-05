11478112c9f64e2ec1b146aedd29de85865c3683
rina0uchi <34464831+rina0uchi@users.noreply.github.com>
2023-12-05 14:33:45 +0900

    rename finction and fixed '__main__'

--- src/pydate_generator/pydate_generator.py
+++ src/pydate_generator/pydate_generator.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta
 
 # Generate dates one by one in order from start to end.
 # timedelta(1) = 1 day, 0:00:00
-def date_generator(start, end):
+def gen_dates(start, end):
     end += timedelta(1)
     step = timedelta(1)
     current = start
@@ -12,8 +12,14 @@ def date_generator(start, end):
 
 
 if __name__ == '__main__':
-    start = date(2023, 12, 23)
-    end = date(2024, 1, 5)
-    
-    for date in date_generator(start, end):
+    print("Enter start date. (e.g. 2023-12-23)")
+    nums = list(map(int, input().split("-")))
+    start = date(nums[0], nums[1], nums[2])
+
+    print("Enter end date. (e.g. 2024-1-3)")
+    nums = list(map(int, input().split("-")))
+    end = date(nums[0], nums[1], nums[2])
+    print("")
+
+    for date in gen_dates(start, end):
         print(date)
